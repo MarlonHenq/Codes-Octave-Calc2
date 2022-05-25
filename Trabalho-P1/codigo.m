f9600858cb1fe2e4de39c45ec07edba8129d1d68
MarlonHenq <marlon@marlonhenq.dev>
2022-05-25 09:56:36 -0300

    trabalho um ajustes e comentários

--- Trabalho-P1/codigo.m
+++ Trabalho-P1/codigo.m
@@ -1,39 +1,63 @@
-clear
-close all
+clear %Deleta as variaveis do workspace
+close all %fecha todas as janelas abertas
 
-[x,y]= meshgrid(-8:0.5:10, -11:0.5:11);
+[x,y]= meshgrid(-11:0.5:11, -11:0.5:11); %Seta a grid de números para X e Y
 
-figure
+figure %Comando para preparar uma janela de figura
 
-%u = e.^(2*x.^2 - y.^2);
-u = x.^2+y.^2-2*x
+subplot(1,2,1) %%SUBPLOT 1
+
+u = 2*x.^2+y.^2-2*x %Equação da curva
 surf(x,y,u) % Plota primeira figura (curva)
-hold on % hold all muda a cor sozinho
-axis([-8 10 -11 11 -1 12]) % axis([xmin xmax ymin ymax zmin zmax])
-hold on
 
-t = meshgrid(-2:0.05:2)
+hold on %Continua desenhando
 
+axis([-11 11 -11 11 -1 6]) % axis([xmin xmax ymin ymax zmin zmax])
 
-x = 3+2/sqrt(13)*t*3
-y = 3+3/sqrt(13)*t*4
+hold on %Continua desenhando
 
-z = 2*x.^2+y.^2-2*x
+t = meshgrid(-10:0.2:2); %Seta a grid de números para t
 
-plot3(x,y,z, '*r')
+%x = 3+2/sqrt(13)*t*3
+%y = 3+3/sqrt(13)*t*4
 
+%z = 2*x.^2+y.^2-2*x
 
-hold on 
+%plot3(x,y,z, '*r')
 
-x = 3+3*t
-y = 3+4*t
+hold on %Continua desenhando
+
+x = 2+0*t
+y = 3+1*t
 z = 2*x.^2+y.^2-2*x
 
 plot3(x,y,z, '*g')
 
 hold on
 
-plot3(3,3,12, '*b')
+plot3(2,3,6, '*b') % Ponto
+
+
+xlabel('eixo x')
+ylabel('eixo y')
+zlabel('eixo z')
+
+subplot(1,2,2) %%SUBPLOT 2
+
+[x,y]= meshgrid(-11:0.5:11, -11:0.5:11);
+u = 2*x.^2+y.^2-2*x
+
+contour3(x,y,u, 40)
+
+hold on
+
+x = 2+0*t
+y = 3+1*t
+z = 2*x.^2+y.^2-2*x
+
+%plot (x,y,"g");
+plot(x,y, ".k");
+
 
 
 xlabel('eixo x')
